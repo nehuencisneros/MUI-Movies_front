e1bc2063cd30aedd29bab1788ea8c338cb29e24a
Nehuen Cisneros <cisneros.n.b@outlook.com>
2023-08-22 22:45:06 -0300

    agreegar cantidad como pop up al to see

--- src/common/NavBar.tsx
+++ src/common/NavBar.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Box , AppBar, Toolbar, Container, Grid, Button, Typography, Stack } from "@mui/material";
+import { Box , AppBar, Toolbar, Container, Grid, Button, Typography, Stack, Badge } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { MoviesToSee } from "./MoviesToSee";
+import { useAppSelector } from "../redux/hooks";
 
 export const NavBar: React.FC<{}> = () => {
     const navigate = useNavigate()
     const [open, setOpen] = React.useState<boolean>(false);
+    const cantMoviesAdded = useAppSelector(state => state.drawer.length)
 
     const handleDrawer = () => {
         setOpen((state) => !state);
@@ -22,7 +24,9 @@ export const NavBar: React.FC<{}> = () => {
                             </Grid>
                             <Grid item>
                                 <Stack spacing={1} direction={"row"}>
-                                    <Button sx={{paddingInline:"10px"}} variant="contained" onClick={()=>handleDrawer()}>Movies To See</Button>
+                                    <Badge badgeContent={cantMoviesAdded} color="error">
+                                        <Button sx={{paddingInline:"10px"}} variant="contained" onClick={()=>handleDrawer()}>Movies To See</Button>
+                                    </Badge>
                                     <Button sx={{paddingInline:"10px"}} variant="outlined">Sing In</Button>
                                 </Stack>
                             </Grid>
