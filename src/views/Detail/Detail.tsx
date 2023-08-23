fd0296b008c09721f3729783bce654f2f425d73e
Nehuen Cisneros <cisneros.n.b@outlook.com>
2023-08-23 15:59:49 -0300

    modularizado de estilos en el detail

--- src/views/Detail/Detail.tsx
+++ src/views/Detail/Detail.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Box, Button, Container, Divider, Grid, Icon, LinearProgress, Typography } from "@mui/material";
+import { Box, Button, Container, Divider, Grid, LinearProgress, Typography } from "@mui/material";
 import { useLoadingBar, useProgressBar } from "../loading";
 import { getMovieById } from "../../redux/slices/moviesSlice";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
@@ -20,7 +20,6 @@ export const DetailView: React.FC<{}> = () => {
     const loading = useLoadingBar();
     
     let params = useParams()
-    
     const id = Number(params.id)
     
     useEffect(()=>{
@@ -37,44 +36,27 @@ export const DetailView: React.FC<{}> = () => {
                     </Box>
                 ) : (
                     <Grid container>
-                        <Box sx={{ position: 'relative', width: '100%', height: 0, paddingTop: '56.25%', }}>
-                            {movieSelector && (
-                                <img src={movieSelector.backdrop_path} alt={movieSelector.title}
-                                    style={{
-                                    position: 'absolute',
-                                    top: 0, left: 0,
-                                    width: '100%', height: '100%',
-                                    objectFit: 'cover',
-                                    }}
-                                />
-                            )}
+                        <Box className={style.boxImage}>
+                            {
+                                movieSelector && <img src={movieSelector.backdrop_path} alt={movieSelector.title} className={style.imagen}/>
+                            }
                             <Grid item sx={{ position: 'absolute', top: '12%', flexDirection: 'column', }}>
                                 <div>
-                                    <Typography variant="h1" align="center"
-                                    sx={{ 
-                                        width: 'fit-content',
-                                        backgroundColor: 'rgba(0, 0, 0, 0.3)', 
-                                        borderRadius: 10, 
-                                        padding: "1%",
-                                        marginInline: '2%'
-                                    }}
-                                    >{movieSelector.title}</Typography>
+                                    <Typography variant="h1" align="center" className={style.title}>
+                                        {movieSelector.title}
+                                    </Typography>
                                 </div>
-                                <Typography variant="h5" mt={2} sx={{ 
-                                        width: 'fit-content',
-                                        backgroundColor: 'rgba(0, 0, 0, 0.15)', 
-                                        padding: '1%',
-                                    }}>
+                                <Typography variant="h5" className={style.overview}>
                                     {movieSelector.overview}
                                 </Typography>
                             </Grid>
-                                <div style={{position: 'absolute', display:"flex", width:'20%',bottom: '5%', marginLeft: "80%",  }}>
-                                    <Button sx={{backgroundColor:"#fc4b08"}} color="inherit" variant="contained">
-                                        <Typography variant="h5" marginY={1}>
+                            <div className={style.divBotonFlotante}>
+                                <Button sx={{backgroundColor:"#fc4b08"}} color="error" variant="contained">
+                                    <Typography variant="h5" marginY={1}>
                                         ADD REVIEW 
-                                        </Typography>
-                                    </Button>
-                                </div>
+                                    </Typography>
+                                </Button>
+                            </div>
                         </Box>
                         { reviewsSelector.length > 0 && 
                             <Grid container sx={{ backgroundColor: '#292f31'}}>
@@ -82,7 +64,7 @@ export const DetailView: React.FC<{}> = () => {
                                 <Divider/>
                                 <Carousel showThumbs={false} className={style.carousel}>
                                     {reviewsSelector.map((data, index) => (
-                                        <Grid item sx={{mb:5, paddingInline:5}} key={index}>
+                                        <Grid item className={style.gridCardReview} key={index}>
                                             <CardReview
                                                 id={data.id}
                                                 author={data.author}
