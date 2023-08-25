2a3520d8758a45e9b85d4d1a669abe62ef85d58c
Nehuen Cisneros <cisneros.n.b@outlook.com>
2023-08-25 14:23:21 -0300

    agregado de modal en review

--- src/views/Detail/index.tsx
+++ src/views/Detail/index.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Box, Button, Container, Divider, Grid, LinearProgress, Typography } from "@mui/material";
+import { Box, Button, Container, Divider, Grid, LinearProgress, Typography, Modal  } from "@mui/material";
 import { useLoadingBar, useProgressBar } from "../loading";
 import { getMovieById } from "../../redux/slices/moviesSlice";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; 
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { CardReview } from "../../components/CardReview/CardReview";
 import style from "./Detail.module.css"
 import { getRevies } from "../../redux/slices/reviewSlice";
@@ -18,29 +18,34 @@ const DetailView: React.FC<{}> = () => {
     //loading
     const progress = useProgressBar();
     const loading = useLoadingBar();
-    
+
     let params = useParams()
     const id = Number(params.id)
-    
-    useEffect(()=>{
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+
+    useEffect(() => {
         dispatch(getRevies(id))
         dispatch(getMovieById(id))
-    },[dispatch,id])
-    
-    return(
+    }, [dispatch, id])
+
+    return (
         <Box >
             <Container maxWidth="xl">
-                { loading ? (
-                    <Box sx={{ width: '100%', mt: 10 }}>
+                {loading ? (
+                    <Box className={style.boxProgress}>
                         <LinearProgress variant="determinate" value={progress} />
                     </Box>
                 ) : (
                     <Grid container>
                         <Box className={style.boxImage}>
                             {
-                                movieSelector && <img src={movieSelector.backdrop_path} alt={movieSelector.title} className={style.imagen}/>
+                                movieSelector && <img src={movieSelector.backdrop_path} alt={movieSelector.title} className={style.imagen} />
                             }
-                            <Grid item sx={{ position: 'absolute', top: '12%', flexDirection: 'column', }}>
+                            <Grid item className={style.textMovieGrid}>
                                 <div>
                                     <Typography variant="h1" align="center" className={style.title}>
                                         {movieSelector.title}
@@ -51,17 +56,27 @@ const DetailView: React.FC<{}> = () => {
                                 </Typography>
                             </Grid>
                             <div className={style.divBotonFlotante}>
-                                <Button sx={{backgroundColor:"#fc4b08"}} color="error" variant="contained">
-                                    <Typography variant="h5" marginY={1}>
-                                        ADD REVIEW 
-                                    </Typography>
+                                <Button sx={{ backgroundColor: "#fc4b08" }} onClick={handleOpen} color="error" variant="contained">
+                                        <Typography variant="h5" marginY={1}>
+                                            ADD REVIEW
+                                        </Typography>
                                 </Button>
+                                    <Modal
+                                        open={open}
+                                        onClose={handleClose}
+                                        aria-labelledby="modal-modal-title"
+                                        aria-describedby="modal-modal-description"
+                                        >
+                                        <Box className={style.modalBox}>
+                                            
+                                        </Box>
+                                    </Modal>
                             </div>
                         </Box>
-                        { reviewsSelector.length > 0 && 
-                            <Grid container sx={{ backgroundColor: '#292f31'}}>
+                        {reviewsSelector.length > 0 &&
+                            <Grid container sx={{ backgroundColor: '#292f31' }}>
                                 <Typography variant="h4" mt={3} ml={2}>Reviews :</Typography>
-                                <Divider/>
+                                <Divider />
                                 <Carousel showThumbs={false} className={style.carousel}>
                                     {reviewsSelector.map((data, index) => (
                                         <Grid item className={style.gridCardReview} key={index}>
@@ -72,14 +87,14 @@ const DetailView: React.FC<{}> = () => {
                                                 rating={data.rating}
                                             />
                                         </Grid>
-                                        ))
+                                    ))
                                     }
                                 </Carousel>
                             </Grid>
                         }
                     </Grid>
                 )
-            }
+                }
             </Container>
         </Box>
     )
