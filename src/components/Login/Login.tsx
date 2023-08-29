6c4bd6742356bf38389fe5ee7611932d0d97c267
Nehuen Cisneros <cisneros.n.b@outlook.com>
2023-08-29 13:44:51 -0300

    Modularizacion del login, funcionando

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Box, Button, Grid, Paper, TextField, Typography } from "@mui/material";
+import { useFormik } from "formik";
+import { useNotification } from "../../context/notification.context"
+import { useAppDispatch } from "../../redux/hooks";
+import { LoginValidate } from "../../utils/validateForm";
+import { loginUser } from "../../redux/slices/loginSlice";
+
+type loginForm = {
+   email: string,
+   password: string
+}
+
+export const Login: React.FC<{}> = () => {
+   const { getSuccess } = useNotification();
+   const dispatch = useAppDispatch()
+
+   const formik = useFormik<loginForm>({
+      initialValues: {
+         email: "",
+         password: "",
+      },
+      validationSchema: LoginValidate,
+      onSubmit: (values: loginForm) => {
+         getSuccess(JSON.stringify(values))
+         dispatch(loginUser(values))
+      }
+   });
+
+   return (
+      <Grid item>
+         <Paper sx={{ padding: "1.2em", borderRadius: "0.5em" }}>
+            <Typography variant="h4" sx={{ mb: 1 }} >Iniciar sesión</Typography>
+            <Box component="form" onSubmit={formik.handleSubmit}>
+               <TextField
+                  name="email"
+                  fullWidth
+                  type="text"
+                  margin="normal"
+                  label="Email"
+                  sx={{ mt: 1, mb: 1 }}
+                  onChange={formik.handleChange}
+                  error={formik.touched.email && Boolean(formik.errors.email)}
+                  helperText={formik.touched.email && formik.errors.email}
+               />
+               <TextField
+                  name="password"
+                  fullWidth
+                  type="password"
+                  margin="normal"
+                  label="Password"
+                  onChange={formik.handleChange}
+                  error={formik.touched.password && Boolean(formik.errors.password)}
+                  helperText={formik.touched.password && formik.errors.password}
+               />
+               <Button
+                  fullWidth
+                  type="submit"
+                  variant="contained"
+                  sx={{ mt: 1, mb: 2 }}
+               >Iniciar sesión</Button>
+            </Box>
+         </Paper>
+      </Grid>
+   )
+}
--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,31 +1,9 @@
 import React, { useState } from "react";
 import { Box, Button, Container, Grid, Paper, TextField, Typography } from "@mui/material"
-import { useNotification } from "../../context/notification.context";
-import { LoginValidate } from "../../utils/validateForm";
-import { useFormik } from "formik";
-import { useAppDispatch } from "../../redux/hooks";
-import { loginUser } from "../../redux/slices/loginSlice";
+import { Login } from "../../components/Login/Login";
 
-type loginForm = {
-    email: string,
-    password: string
-}
 
 const LoginView: React.FC<{}> = () => {
-    const { getSuccess } = useNotification();
-    const dispatch = useAppDispatch()
-
-    const formik = useFormik<loginForm>({
-        initialValues: {
-            email: "",
-            password: "",
-        },
-        validationSchema: LoginValidate,
-        onSubmit: (values: loginForm) => {
-            getSuccess(JSON.stringify(values))
-            dispatch(loginUser(values))
-        }
-    });
 
     return (
         <Container maxWidth="sm" >
@@ -36,41 +14,7 @@ const LoginView: React.FC<{}> = () => {
                 justifyContent="center"
                 sx={{ minHeight: "100vh" }}
             >
-                <Grid item>
-                    <Paper sx={{ padding: "1.2em", borderRadius: "0.5em" }}>
-                        <Typography variant="h4" sx={{ mb: 1 }} >Iniciar sesión</Typography>
-                        <Box component="form" onSubmit={formik.handleSubmit}>
-                            <TextField
-                                name="email"
-                                fullWidth
-                                type="text"
-                                margin="normal"
-                                label="Email"
-                                sx={{ mt: 1, mb: 1 }}
-                                onChange={formik.handleChange}
-                                error={formik.touched.email && Boolean(formik.errors.email)}
-                                helperText={formik.touched.email && formik.errors.email}
-                            />
-                            <TextField
-                                name="password"
-                                fullWidth
-                                type="password"
-                                margin="normal"
-                                label="Password"
-                                sx={{ mt: 1, mb: 1 }}
-                                onChange={formik.handleChange}
-                                error={formik.touched.password && Boolean(formik.errors.password)}
-                                helperText={formik.touched.password && formik.errors.password}
-                            />
-                            <Button
-                                fullWidth
-                                type="submit"
-                                variant="contained"
-                                sx={{ mt: 1, mb: 2 }}
-                            >Iniciar sesión</Button>
-                        </Box>
-                    </Paper>
-                </Grid>
+                <Login/>
             </Grid>
         </Container>
     );
